37bd9c59127b809c910ab5d758b0dc282e498d29
Henrixounez <henrix.ounez@yahoo.fr>
2021-08-01 01:30:06 +0200

    [ADD]: Re-render Canvas on grid visibility change + aligned checkbox row in parameters

--- pages/ui/modals/Parameters.tsx
+++ pages/ui/modals/Parameters.tsx
@@ -1,6 +1,18 @@
+import styled from 'styled-components';
 import { useDispatch, useSelector } from "react-redux";
 import { SET_GRID_ACTIVE } from '../../../store/actions/parameters';
 import { ReduxState } from '../../../store';
+import { getCanvasController } from "../../controller/CanvasController";
+
+const CheckboxRow = styled.div`
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  user-select: none;
+`;
 
 export default function ModalParameters() {
     const dispatch = useDispatch();
@@ -8,7 +20,7 @@ export default function ModalParameters() {
 
     const changeGridActive = () => {
         dispatch({type: SET_GRID_ACTIVE, payload: !gridActive});
-        
+        getCanvasController()?.render();
     }
     return (
         <>
@@ -16,9 +28,10 @@ export default function ModalParameters() {
                 Parameters
             </h3>
             <hr/>
-            Grid active
-            <br/>
-            <input type="checkbox" id="gridActive" checked={gridActive} onChange={changeGridActive}/>
+            <CheckboxRow onClick={changeGridActive}>
+                Grid active
+                <input type="checkbox" id="gridActive" checked={gridActive} readOnly/>
+            </CheckboxRow>
         </>
     );
 }
