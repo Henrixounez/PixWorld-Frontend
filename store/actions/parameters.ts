e9f6a658d9645b4d0a1abf52433713eca2628f89
Henrixounez <henrix.ounez@yahoo.fr>
2021-08-02 03:46:03 +0200

    [ADD]: Pixel Activity + Activity Parameter

--- store/actions/parameters.ts
+++ store/actions/parameters.ts
@@ -3,6 +3,7 @@ import { ReduxState } from "..";
 /* Action */
 export const SET_GRID_ACTIVE = 'SET_GRID_ACTIVE';
 export const SET_ZOOM_TOWARD_CURSOR = 'SET_ZOOM_TOWARD_CURSOR';
+export const SET_ACTIVITY = 'SET_ACTIVITY';
 
 /* Types */
 
@@ -10,13 +11,16 @@ export interface SetGridActiveAction {
     type: typeof SET_GRID_ACTIVE;
     payload: boolean;
 }
-
 export interface SetZoomTowardCursorAction {
     type: typeof SET_ZOOM_TOWARD_CURSOR;
     payload: boolean;
 }
+export interface SetActivityAction {
+    type: typeof SET_ACTIVITY;
+    payload: boolean;
+}
 
-export type Actions = SetGridActiveAction | SetZoomTowardCursorAction;
+export type Actions = SetGridActiveAction | SetZoomTowardCursorAction | SetActivityAction;
 
 /* Functions */
 export function setGridActive(state: ReduxState, action: SetGridActiveAction): ReduxState {
@@ -26,12 +30,18 @@ export function setGridActive(state: ReduxState, action: SetGridActiveAction): R
         gridActive: action.payload
     };
 }
-
 export function setZoomTowardCursor(state: ReduxState, action: SetZoomTowardCursorAction): ReduxState {
     localStorage.setItem('zoomTowardCursor', String(action.payload));
     return {
         ...state,
         zoomTowardCursor: action.payload
+    };
+}
+export function setActivity(state: ReduxState, action: SetActivityAction): ReduxState {
+    localStorage.setItem('activity', String(action.payload));
+    return {
+        ...state,
+        activity: action.payload
     };
 }
 
@@ -44,5 +54,9 @@ export const dispatches = [
     {
         action: SET_ZOOM_TOWARD_CURSOR,
         function: setZoomTowardCursor
+    },
+    {
+        action: SET_ACTIVITY,
+        function: setActivity
     }
 ]
