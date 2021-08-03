cf10abc561bc152b78fefd4e7a125145bfa8baca
Henrixounez <henrix.ounez@yahoo.fr>
2021-08-03 03:00:56 +0200

    [ADD]: Updated icons

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -20,11 +20,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width,initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
         <meta name='format-detection' content='telephone=no' />
         <meta name='mobile-web-app-capable' content='yes' />
-        <meta name='msapplication-TileColor' content='#2B5797' />
+        <meta name='msapplication-TileColor' content='#da532c' />
         <meta name='msapplication-tap-highlight' content='no' />
-        <meta name='theme-color' content='#000000' />
+        <meta name='theme-color' content='#FFFFFF' />
 
-        <link rel='apple-touch-icon' href='/icons/apple-touch-icon.png' />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
 
         <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
         <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
