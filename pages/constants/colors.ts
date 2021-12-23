aee3e9f18af73ed9fffb26f728a6267517a35dd5
Henrixounez <henrix.ounez@yahoo.fr>
2021-12-23 17:42:06 +0100

    [ADD]: Christmas colors and icons

--- pages/constants/colors.ts
+++ pages/constants/colors.ts
@@ -10,11 +10,35 @@ export enum Colors {
   ALERT,
 }
 
+// const colors = {
+//   [Colors.UI_BACKGROUND]: "#FFFD",
+//   [Colors.UI_BACKDROP]: "#FFFA",
+//   [Colors.UI_BORDER]: "#000",
+//   [Colors.TEXT]: "#000",
+//   [Colors.LIGHT_TEXT]: '#444',
+//   [Colors.UNREAD]: "#FF0000",
+//   [Colors.LINK]: "#428BCA",
+//   [Colors.HOVERED_LINK]: "#226BAA",
+//   [Colors.ALERT]: "#FFA9A9cc",
+// };
+
+// const darkThemeColors = {
+//   [Colors.UI_BACKGROUND]: "#000D",
+//   [Colors.UI_BACKDROP]: "#000A",
+//   [Colors.UI_BORDER]: "#FFF",
+//   [Colors.TEXT]: "#FFF",
+//   [Colors.LIGHT_TEXT]: '#AAA',
+//   [Colors.UNREAD]: "#FF0000",
+//   [Colors.LINK]: "#428BCA",
+//   [Colors.HOVERED_LINK]: "#226BAA",
+//   [Colors.ALERT]: "#FFA9A9cc",
+// };
+
 const colors = {
-  [Colors.UI_BACKGROUND]: "#FFFD",
+  [Colors.UI_BACKGROUND]: "#ff4949",
   [Colors.UI_BACKDROP]: "#FFFA",
-  [Colors.UI_BORDER]: "#000",
-  [Colors.TEXT]: "#000",
+  [Colors.UI_BORDER]: "#FFF",
+  [Colors.TEXT]: "#FFF",
   [Colors.LIGHT_TEXT]: '#444',
   [Colors.UNREAD]: "#FF0000",
   [Colors.LINK]: "#428BCA",
@@ -25,8 +49,8 @@ const colors = {
 const darkThemeColors = {
   [Colors.UI_BACKGROUND]: "#000D",
   [Colors.UI_BACKDROP]: "#000A",
-  [Colors.UI_BORDER]: "#FFF",
-  [Colors.TEXT]: "#FFF",
+  [Colors.UI_BORDER]: "#ff8181",
+  [Colors.TEXT]: "#ff8181",
   [Colors.LIGHT_TEXT]: '#AAA',
   [Colors.UNREAD]: "#FF0000",
   [Colors.LINK]: "#428BCA",
