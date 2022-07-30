962db523cf48e3f784a6907b1be46488253204a3
Henrixounez <henrix.ounez@yahoo.fr>
2022-07-30 20:13:05 +0200

    chores: remove christmas theme

--- pages/constants/colors.ts
+++ pages/constants/colors.ts
@@ -10,35 +10,11 @@ export enum Colors {
   ALERT,
 }
 
-// const colors = {
-//   [Colors.UI_BACKGROUND]: "#FFFD",
-//   [Colors.UI_BACKDROP]: "#FFFA",
-//   [Colors.UI_BORDER]: "#000",
-//   [Colors.TEXT]: "#000",
-//   [Colors.LIGHT_TEXT]: '#444',
-//   [Colors.UNREAD]: "#FF0000",
-//   [Colors.LINK]: "#428BCA",
-//   [Colors.HOVERED_LINK]: "#226BAA",
-//   [Colors.ALERT]: "#FFA9A9cc",
-// };
-
-// const darkThemeColors = {
-//   [Colors.UI_BACKGROUND]: "#000D",
-//   [Colors.UI_BACKDROP]: "#000A",
-//   [Colors.UI_BORDER]: "#FFF",
-//   [Colors.TEXT]: "#FFF",
-//   [Colors.LIGHT_TEXT]: '#AAA',
-//   [Colors.UNREAD]: "#FF0000",
-//   [Colors.LINK]: "#428BCA",
-//   [Colors.HOVERED_LINK]: "#226BAA",
-//   [Colors.ALERT]: "#FFA9A9cc",
-// };
-
 const colors = {
-  [Colors.UI_BACKGROUND]: "#ff4949",
+  [Colors.UI_BACKGROUND]: "#FFFD",
   [Colors.UI_BACKDROP]: "#FFFA",
-  [Colors.UI_BORDER]: "#FFF",
-  [Colors.TEXT]: "#FFF",
+  [Colors.UI_BORDER]: "#000",
+  [Colors.TEXT]: "#000",
   [Colors.LIGHT_TEXT]: '#444',
   [Colors.UNREAD]: "#FF0000",
   [Colors.LINK]: "#428BCA",
@@ -49,14 +25,39 @@ const colors = {
 const darkThemeColors = {
   [Colors.UI_BACKGROUND]: "#000D",
   [Colors.UI_BACKDROP]: "#000A",
-  [Colors.UI_BORDER]: "#ff8181",
-  [Colors.TEXT]: "#ff8181",
+  [Colors.UI_BORDER]: "#FFF",
+  [Colors.TEXT]: "#FFF",
   [Colors.LIGHT_TEXT]: '#AAA',
   [Colors.UNREAD]: "#FF0000",
   [Colors.LINK]: "#428BCA",
   [Colors.HOVERED_LINK]: "#226BAA",
   [Colors.ALERT]: "#FFA9A9cc",
 };
+
+// Christmas Colors
+// const colors = {
+//   [Colors.UI_BACKGROUND]: "#ff4949",
+//   [Colors.UI_BACKDROP]: "#FFFA",
+//   [Colors.UI_BORDER]: "#FFF",
+//   [Colors.TEXT]: "#FFF",
+//   [Colors.LIGHT_TEXT]: '#444',
+//   [Colors.UNREAD]: "#FF0000",
+//   [Colors.LINK]: "#428BCA",
+//   [Colors.HOVERED_LINK]: "#226BAA",
+//   [Colors.ALERT]: "#FFA9A9cc",
+// };
+
+// const darkThemeColors = {
+//   [Colors.UI_BACKGROUND]: "#000D",
+//   [Colors.UI_BACKDROP]: "#000A",
+//   [Colors.UI_BORDER]: "#ff8181",
+//   [Colors.TEXT]: "#ff8181",
+//   [Colors.LIGHT_TEXT]: '#AAA',
+//   [Colors.UNREAD]: "#FF0000",
+//   [Colors.LINK]: "#428BCA",
+//   [Colors.HOVERED_LINK]: "#226BAA",
+//   [Colors.ALERT]: "#FFA9A9cc",
+// };
 
 export function getColor(colorName: Colors, darkMode: boolean) {
   return darkMode ? darkThemeColors[colorName] : colors[colorName];
