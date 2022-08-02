5d2675dfc262e41b44b6d267d949e26fb06863e0
Henrixounez <henrix.ounez@yahoo.fr>
2022-08-03 00:28:56 +0200

    fix: locale for tr in next config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'fr', 'ptbr', 'ru', 'es'],
+    locales: ['en', 'fr', 'ptbr', 'ru', 'es', 'tr'],
   },
   localePath: path.resolve('./public/locales'),
 };
