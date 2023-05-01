2c9e7623c344eb531882932ad9849aa7b5f5d71c
Henrixounez <henrix.ounez@yahoo.fr>
2023-05-01 20:50:00 +0200

    feat: quick ban panel from coordinate pixel logs panel

--- pages/index/ui/modals/Moderation/Logs.tsx
+++ pages/index/ui/modals/Moderation/Logs.tsx
@@ -1,12 +1,15 @@
 import axios from "axios";
 import { FormEvent, useState } from "react";
-import { Search } from "react-feather";
+import { Search, UserX, Send } from "react-feather";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
+import { format } from "date-fns";
+
 import { API_URL } from "../../../../constants/api";
 import { onCoordinatesPaste } from "../../../../pagesComponents"
 import { ReduxState } from "../../../store";
-import { ModalBoxContainer, ModalBoxTitle, ModalCoordRow, ModalQueryForm, ModalTextfield } from "./components";
+import { ModalBoxContainer, ModalBoxTitle, ModalCoordRow, ModalQueryForm, ModalTextfield, ModalErrorBox } from "./components";
+import { getColor, Colors } from "../../../../constants/colors";
 
 export const ContainerLogsResult = styled.div`
   max-height: 500px;
@@ -54,12 +57,69 @@ function dateFormat(date: string) {
   return `${d.toLocaleDateString('fr-FR')} ${d.toLocaleTimeString('fr-FR')}`;
 }
 
+function PixelLogBan({
+  ip,
+  username,
+  close,
+}: {
+  ip: string;
+  username?: string;
+  close: () => void;
+}) {
+  const [reason, setReason] = useState("");
+  const [date, setDate] = useState("");
+  const [error, setError] = useState("");
+  const darkMode = useSelector((state: ReduxState) => state.darkMode);
+
+  const banUser = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!date) {
+      setError("No date specified");
+      return
+    }
+    try {
+      setError("");
+      const token = localStorage.getItem('token');
+      await axios.post(`${API_URL}/admin/banUser`, { username, ban: true, reason, date }, { headers: { 'Authorization': token } });
+      await axios.post(`${API_URL}/admin/ban`, { ip, ban: true, reason, date }, { headers: { 'Authorization': token } });
+      setReason("");
+      setDate("");
+      close();
+    } catch (err: any) {
+      setError(err.response.data || err.message);
+    }
+  }
+
+  console.log("ici")
+  return (
+    <tr>
+      <td colSpan={7}>
+        <ModalQueryForm onSubmit={banUser} darkMode={darkMode}>
+          <ModalCoordRow darkMode={darkMode}>
+            <ModalTextfield placeholder="Reason" type="text" value={reason} onChange={(e) => setReason(e.target.value)}  darkMode={darkMode}/>
+            <ModalTextfield type="datetime-local" value={date} min={format(new Date(), "yyyy-MM-dd'T'hh:mm")} step={60} onChange={(e) => setDate(e.target.value)}  darkMode={darkMode}/>
+            <button>
+              <Send/>
+            </button>
+          </ModalCoordRow>
+        </ModalQueryForm>
+        { error ? (
+          <ModalErrorBox darkMode={darkMode}>
+            {error}
+          </ModalErrorBox>
+        ) : null}
+      </td>
+    </tr>
+  );
+}
+
 function PixelLogs() {
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
   const [canvas, setCanvas] = useState("");
   const [results, setResults] = useState<PixelLogResult[]>([]);
   const darkMode = useSelector((state: ReduxState) => state.darkMode);
+  const [banLine, setBanLine] = useState<number | undefined>(undefined);
 
   const searchPixelLogs = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -100,10 +160,12 @@ function PixelLogs() {
                 <th>Username</th>
                 <th>Position</th>
                 <th>Color</th>
+                <th>Ban</th>
               </tr>
             </thead>
             <tbody>
               {results.map((r, i) => (
+                <>
                 <tr key={i}>
                   <td>{dateFormat(r.createdAt)}</td>
                   <td>{r.ip}</td>
@@ -114,7 +176,20 @@ function PixelLogs() {
                     <div style={{ display: "inline-block", height: "10px", width: "10px", backgroundColor: r.color, marginRight: ".5rem" }}/>
                     {r.color}
                   </td>
+                  <td onClick={
+                    () => setBanLine(banLine === i ? undefined : i)
+                  }>
+                    <UserX color={getColor(Colors.UNREAD, darkMode)} style={{ cursor: "pointer" }}/>
+                  </td>
                 </tr>
+                { banLine === i && (
+                  <PixelLogBan
+                    ip={r.ip}
+                    username={r.username}
+                    close={() => setBanLine(undefined)}
+                  />
+                )}
+                </>
               ))}
             </tbody>
           </LogsResults>
